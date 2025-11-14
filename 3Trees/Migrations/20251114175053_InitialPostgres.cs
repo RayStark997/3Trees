4c251a9843e52b060992484a8bbe6ef76b632710
Windows 11 <Windows 11@DESKTOP-HA70KOO>
2025-11-14 15:10:20 -0300

    Versão render

---
+++ 3Trees/Migrations/20251114175053_InitialPostgres.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace _3Trees.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trilhas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Coordenada = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Descricao = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Cidade = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CaminhoImagem = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trilhas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trilhas");
+        }
+    }
+}
